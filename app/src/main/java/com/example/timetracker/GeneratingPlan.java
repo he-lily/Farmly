94eaded1fb8cabbdc3840a40e48760b48c77c785
Jeffrey <jeffrch3@uci.edu>
2021-02-18 05:55:34 -0800

    First Push

---
+++ app/src/main/java/com/example/timetracker/GeneratingPlan.java
@@ -0,0 +1,14 @@
+package com.example.timetracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GeneratingPlan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generating_plan);
+    }
+}
