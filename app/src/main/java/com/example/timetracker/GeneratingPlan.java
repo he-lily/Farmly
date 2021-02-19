1655d3730745e9d666cb5d4c145bd4345b075f77
he-lily <lmhe@uci.edu>
2021-02-19 00:45:11 -0800

    bottomNavView finished and working
    bottomNavView functioning properly. using fill in pages to be changed later

--- app/src/main/java/com/example/timetracker/GeneratingPlan.java
+++ app/src/main/java/com/example/timetracker/GeneratingPlan.java
@@ -2,7 +2,10 @@ package com.example.timetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GeneratingPlan extends AppCompatActivity {
 
@@ -10,6 +13,15 @@ public class GeneratingPlan extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generating_plan);
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view){
+                Intent intent = new Intent(GeneratingPlan.this, BottomNavView.class);
+                startActivity(intent);
+            }
+        });
     }
 }
 
