239cdf52172eb7480977e54bc83f8442a9fddead
Jeffrey <jeffrch3@uci.edu>
2021-03-10 06:36:04 -0800

    COMMIT JEFF 3/10 6AM

---
+++ app/src/main/java/com/example/timetracker/LimitedAppsAdapter.java
@@ -0,0 +1,73 @@
+package com.example.timetracker;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class LimitedAppsAdapter extends RecyclerView.Adapter<LimitedAppsAdapter.ViewHolder> {
+    ArrayList<String> mAppNames = new ArrayList<>();
+    ArrayList<String> mAppTimes = new ArrayList<>();
+    Context mContext;
+
+    public LimitedAppsAdapter(Context context, ArrayList<String> appNames, ArrayList<String> appTimes){
+        mContext = context;
+        mAppNames = appNames;
+        mAppTimes = appTimes;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_limited_apps, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.limitedApp.setText(mAppNames.get(position));
+        holder.limitedTime.setText(mAppTimes.get(position));
+        holder.limitedBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                holder.limitedTime.setText("" + progress + " mins");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mAppNames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView limitedApp;
+        TextView limitedTime;
+        SeekBar limitedBar;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            limitedApp = itemView.findViewById(R.id.limitedApp);
+            limitedTime = itemView.findViewById(R.id.limitedTime);
+            limitedBar = itemView.findViewById(R.id.limitedBar);
+        }
+    }
+}
--- app/src/main/java/com/example/timetracker/SetLimits.java
+++ app/src/main/java/com/example/timetracker/SetLimits.java
@@ -1,19 +1,34 @@
 package com.example.timetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+
 public class SetLimits extends AppCompatActivity {
     Button generatingPlan;
+    RecyclerView rv;
+    ArrayList<String> mAppNames = new ArrayList<>();
+    ArrayList<String> mAppTimes = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_limits);
-
+        /** THIS IS THE CODE FOR DISPLAYING THE RECYCLER VIEW FOR THE SET LIMITS,
+         * I HAVEN'T IMPORTED THE LIMITED APPS YET BUT I'LL DO THAT SOON.
+         * THE ADAPTER AND LAYOUT FOR IT SHOULD BE GOOD THOUGH.
+        rv = findViewById(R.id.recyclerView2);
+        LimitedAppsAdapter ad = new LimitedAppsAdapter(this, mAppNames, mAppTimes);
+        rv.setAdapter(ad);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+         **/
         /** Added button functionality here so we can get to the next activity. Change anything if you need to. **/
         generatingPlan = findViewById(R.id.save_limits);
         generatingPlan.setOnClickListener(new View.OnClickListener() {
