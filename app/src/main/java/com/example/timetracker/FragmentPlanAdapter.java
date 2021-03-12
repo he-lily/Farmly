a9ff6f9dddea53658eba6ddf39914816a9d4a5c1
Jeffrey <jeffrch3@uci.edu>
2021-03-12 05:30:53 -0800

    Jeff commit 3/12 5am

--- app/src/main/java/com/example/timetracker/FragmentPlanAdapter.java
+++ app/src/main/java/com/example/timetracker/FragmentPlanAdapter.java
@@ -1,4 +1,65 @@
 package com.example.timetracker;
 
-public class FragmentPlanAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class FragmentPlanAdapter extends RecyclerView.Adapter<FragmentPlanAdapter.fpViewHolder>{
+    List<String> mCatNames;
+    List<Double> mCatTimes;
+    List<ProgressBar> mCatBar;
+    Context mContext;
+
+    public FragmentPlanAdapter(Context context, List<String> catNames, List<Double> catTimes, List<ProgressBar> catBar){
+        mContext = context;
+        mCatNames = catNames;
+        mCatTimes = catTimes;
+        mCatBar = catBar;
+    }
+
+    @NonNull
+    @Override
+    public fpViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_fragment_plan, parent, false);
+        fpViewHolder holder = new fpViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull fpViewHolder holder, int position) {
+        holder.catName.setText(mCatNames.get(position));
+        holder.catTime.setText(mCatTimes.get(position).toString() + " mins");
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCatNames.size();
+    }
+
+    private void updateProgress(@NonNull fpViewHolder holder, int position){
+        double dt = mCatTimes.get(position) / 60; // Divide by 60 (mins) as arbitrary value for now
+        int it = (int)dt * 100; // Multiply by 100 to get whole number value (int)
+        holder.catBar.setProgress(it);
+    }
+
+    public class fpViewHolder extends RecyclerView.ViewHolder {
+        public TextView catName;
+        public TextView catTime;
+        public ProgressBar catBar;
+
+        public fpViewHolder(@NonNull View itemView) {
+            super(itemView);
+            catName = itemView.findViewById(R.id.catName);
+            catTime = itemView.findViewById(R.id.catTime);
+            catBar = itemView.findViewById(R.id.catBar);
+        }
+    }
 }
--- app/src/main/java/com/example/timetracker/SetLimits.java
+++ app/src/main/java/com/example/timetracker/SetLimits.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 public class SetLimits extends AppCompatActivity {
     Button generatingPlan;
-    RecyclerView recyclerView ;
+    RecyclerView recyclerView;
     List<String> mAppNames;
     List<Double> mAppTimes = new ArrayList<>();
 
@@ -23,16 +23,8 @@ public class SetLimits extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_limits);
-        /** THIS IS THE CODE FOR DISPLAYING THE RECYCLER VIEW FOR THE SET LIMITS,
-         * I HAVEN'T IMPORTED THE LIMITED APPS YET BUT I'LL DO THAT SOON.
-         * THE ADAPTER AND LAYOUT FOR IT SHOULD BE GOOD THOUGH.
-        rv = findViewById(R.id.recyclerView2);
-        LimitedAppsAdapter ad = new LimitedAppsAdapter(this, mAppNames, mAppTimes);
-        rv.setAdapter(ad);
-        rv.setLayoutManager(new LinearLayoutManager(this));
-         **/
+
         /** Added button functionality here so we can get to the next activity. Change anything if you need to. **/
-            
         mAppNames = HoldUserInfo.getInstance().getUser_disliked_apps();
         int index = 0;
         HashMap<String,Double> app_usage = (HashMap<String,Double>)HoldUserInfo.getInstance().getUser_app_usage().get(4);
